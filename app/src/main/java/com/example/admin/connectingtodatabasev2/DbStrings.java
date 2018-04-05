4b00133fe395748e9e94c97995546f57fd975dd0
Piotrek <piotrjazwinski94@gmail.com>
2018-04-05 11:35:58 +0200

    Basic connection to database system complete

---
+++ app/src/main/java/com/example/admin/connectingtodatabasev2/DbStrings.java
@@ -0,0 +1,14 @@
+package com.example.admin.connectingtodatabasev2;
+
+/**
+ * Created by Admin on 04.04.2018.
+ */
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "10.0.2.2:3306";
+    static final String DATABASE_NAME = "fruits";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "root";
+
+}
