3f6d8f30413a974fc62c51b9720b0fc4297bd651
Piotrek <piotrjazwinski94@gmail.com>
2018-04-18 07:50:19 +0200

    2nd listliew is clicable and works but not completely

---
+++ app/src/main/java/com/example/admin/connectingtodatabasev2/monstersItemAdapter.java
@@ -0,0 +1,10 @@
+package com.example.admin.connectingtodatabasev2;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Admin on 16.04.2018.
+ */
+
+public class monstersItemAdapter extends BaseAdapter {
+}
